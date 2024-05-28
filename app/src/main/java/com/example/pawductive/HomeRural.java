2bd85b0a8f1235607a9bd9d1dacd155eb3426097
Aizy <aizernerevangelista@gmail.com>
2024-05-29 05:26:40 +0800

    settings

---
+++ app/src/main/java/com/example/pawductive/HomeRural.java
@@ -0,0 +1,16 @@
+package com.example.pawductive;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeRural extends AppCompatActivity {
+    ImageView background;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_rural);
+    }
+}
--- app/src/main/java/com/example/pawductive/Menu.java
+++ app/src/main/java/com/example/pawductive/Menu.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Menu extends AppCompatActivity {
     ImageView appLogo;
-
+    LinearLayout btnHome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_menu);
+        btnHome = findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this, HomeRural.class);
+                startActivity(intent);
+            }
+        });
     }
 }
